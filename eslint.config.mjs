bf762ddbdec90e9828d4a91a37f77be47da3152e
sushildahiya3 <sushil@vipas.ai>
2024-08-12 18:38:46 +0530

    gitignore added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,21 +4,21 @@ import pluginJs from "@eslint/js";
 
 export default [
   {
-    rules:{"no-undef-init": [
-      "error"
-  ],
-  "no-undefined": [
-      "error"
-  ],
-  "no-unreachable": [
-      "error"
-  ],
-  "no-debugger": [
-      "error"
-  ],
-  "no-undef": [
-      "error"
-  ]},
+  //   rules:{"no-undef-init": [
+  //     "error"
+  // ],
+  // "no-undefined": [
+  //     "error"
+  // ],
+  // "no-unreachable": [
+  //     "error"
+  // ],
+  // "no-debugger": [
+  //     "error"
+  // ],
+  // "no-undef": [
+  //     "error"
+  // ]},
     languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  // pluginJs.configs.recommended,
 ];
